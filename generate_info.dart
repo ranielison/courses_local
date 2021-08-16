887fd50a6456b23223d168988d9b4550ad68b4a5
Raniélison <ranielisonsoares2@gmail.com>
2021-08-15 23:59:47 -0300

    feat: creating function to generate

--- generate_info.dart
+++ generate_info.dart
@@ -1,12 +1,24 @@
 import 'dart:io';
 import 'lib/src/global/data/models/course.dart';
+import 'lib/src/global/data/models/module.dart';
 
 class GenerateInfo {
   static Course generate(String path) {
+    List<Module> modules = [];
+
+    Course(
+      name: '',
+      folderPath: '',
+      modules: [],
+    );
+
     Directory courseDir = Directory(path);
 
-    courseDir.list(recursive: true).forEach((item) {
-      print(item);
+    courseDir.list(recursive: true).forEach((FileSystemEntity item) {
+      if (item is Directory) {}
+      //print(item.runtimeType);
+      //print('Is File: ${item is File}');
+      //print('Is Directory: ${item is Directory}');
     });
 
     return Course(
